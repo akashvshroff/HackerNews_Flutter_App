a01708618d7b4b88b39d7f5540453ed63275666a
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-02 10:47:15 +0530

    BLOC integration

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/stories_bloc.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(
         title: Text("TOP NEWS"),
       ),
-      body: Text('Show a list.'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text("Still waiting on ids");
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.lenght,
+          itemBuilder: (context, int index) {
+            return Text(snapshot.data[index]);
+          },
+        );
+      },
     );
   }
 }
