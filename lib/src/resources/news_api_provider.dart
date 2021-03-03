15846613d4d1a1482dd51b0355b326d68ca8275d
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-03 15:28:20 +0530

    API endpoint fix

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -6,19 +6,20 @@ import '../models/item_model.dart';
 import 'dart:async';
 import 'repository.dart';
 
-final root = 'http://hacker-news.firebaseio.com/v0';
+final root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider implements Source {
   Client client = Client();
 
   Future<List<int>> fetchTopIds() async {
-    final response = await client.get('$root/topstories.json?print=pretty');
+    final response = await client.get('$root/topstories.json');
     final ids = jsonDecode(response.body);
     return ids.cast<int>();
   }
 
   Future<ItemModel> fetchItem(int id) async {
-    final response = await client.get('$root/item/$id.json?print=pretty');
+    final response = await client.get('$root/item/$id.json');
+    print(response.body);
     final parsedJson = json.decode(response.body);
     return ItemModel.fromJson(parsedJson);
   }
