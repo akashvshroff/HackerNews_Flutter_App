26e650854a5f67f87400040568f3c4ff6f4dca2e
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-09 10:16:06 +0530

    Parsing route data

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  NewsDetail({this.itemId});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("News Detail")),
+      body: Text("$itemId"),
     );
   }
 }
