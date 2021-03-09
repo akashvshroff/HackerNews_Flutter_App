e2e9d849bd7b1a5cba60a45c8a98ee83984831aa
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-09 10:00:24 +0530

    Added basic navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
+import 'screens/news_detail.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'News',
-      home: StoriesProvider(
-        child: NewsList(),
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'News',
+        home: NewsList(),
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+    } else {
+      return MaterialPageRoute(builder: (context) {
+        return NewsDetail();
+      });
+    }
   }
 }
