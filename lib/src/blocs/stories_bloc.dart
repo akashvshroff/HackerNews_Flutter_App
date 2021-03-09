dcd3455791850922797cc6a1da4d0dcc5e79cc29
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-09 12:55:15 +0530

    Refactor

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -8,46 +8,17 @@ class StoriesBloc {
   final _topIds = PublishSubject<List<int>>();
   final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
   final _itemsFetcher = PublishSubject<int>();
-  // final _queryItems = BehaviorSubject<String>();
 
   // getter to Stream
   Stream<List<int>> get topIds => _topIds.stream;
   Stream<Map<int, Future<ItemModel>>> get items => _itemsOutput.stream;
-  // Stream<String> get queryStream => _queryItems.stream;
 
   //getters to Sink
   Function(int) get fetchItem => _itemsFetcher.sink.add;
-  // Function(String) get addQuery => _queryItems.sink.add;
 
   StoriesBloc() {
     _itemsFetcher.stream.transform(_itemsTransformer()).pipe(_itemsOutput);
   }
-
-  // void queryIds(String query) async {
-  //   if (query.isEmpty || query == null) {
-  //     fetchTopIds();
-  //   }
-  //   _topIds.add(null);
-  //   query = query.toLowerCase();
-  //   final List<int> resultIds = [];
-  //   final topIds = await _repository.fetchTopIds();
-  //   print('${topIds.length}');
-  //   for (int id in topIds) {
-  //     // print('$id');
-  //     ItemModel item = await _repository.fetchItem(id);
-  //     if (item.title.toLowerCase().contains(query)) {
-  //       resultIds.add(id);
-  //     }
-  //   }
-  //   if (resultIds.isEmpty) {
-  //     print('no items');
-  //     _queryItems.sink.addError('No such item exists');
-  //     _topIds.sink.add(topIds);
-  //   } else {
-  //     _queryItems.sink.add(query);
-  //     _topIds.sink.add(resultIds);
-  //   }
-  // }
 
   void fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -69,7 +40,6 @@ class StoriesBloc {
   }
 
   void dispose() {
-    // _queryItems.close();
     _itemsFetcher.close();
     _itemsOutput.close();
     _topIds.close();
