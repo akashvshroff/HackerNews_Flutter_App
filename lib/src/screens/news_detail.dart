69416fef1d73c298aa7e51c57f8a50accc14ca51
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-15 22:11:04 +0530

    Stylistic changes and comments logic

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -12,7 +12,7 @@ class NewsDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = CommentsProvider.of(context);
     return Scaffold(
-      appBar: AppBar(title: Text("News Detail")),
+      appBar: AppBar(title: Text("NEWS DETAIL")),
       body: buildBody(bloc),
     );
   }
@@ -32,10 +32,32 @@ class NewsDetail extends StatelessWidget {
               return Text("Loading");
             }
 
-            return Text(itemSnapshot.data.title);
+            return buildList(itemSnapshot.data, snapshot.data);
           },
         );
       },
     );
+  }
+
+  Widget buildList(ItemModel item, Map<int, Future<ItemModel>> itemMap) {
+    return ListView(
+      children: <Widget>[
+        buildTitle(item),
+      ],
+    );
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+        alignment: Alignment.topCenter,
+        margin: EdgeInsets.all(10.0),
+        child: Text(
+          item.title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ));
   }
 }
