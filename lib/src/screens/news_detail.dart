ccd2ce9c3a82207035c80883cb978bb0bf941c31
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2021-03-18 15:41:15 +0530

    Stylistic edits

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import '../blocs/comments_provider.dart';
 import '../models/item_model.dart';
 import 'dart:async';
 import '../widgets/comment.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -13,7 +16,7 @@ class NewsDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = CommentsProvider.of(context);
     return Scaffold(
-      appBar: AppBar(title: Text("NEWS DETAIL")),
+      appBar: AppBar(title: Text("STORY")),
       body: buildBody(bloc),
     );
   }
@@ -23,14 +26,14 @@ class NewsDetail extends StatelessWidget {
       stream: bloc.itemWithComments,
       builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
         if (!snapshot.hasData) {
-          return Text("Loading");
+          return Center(child: CircularProgressIndicator());
         }
         final itemFuture = snapshot.data[itemId];
         return FutureBuilder(
           future: itemFuture,
           builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
             if (!itemSnapshot.hasData) {
-              return Text("Loading");
+              return Center(child: CircularProgressIndicator());
             }
 
             return buildList(itemSnapshot.data, snapshot.data);
@@ -43,6 +46,8 @@ class NewsDetail extends StatelessWidget {
   Widget buildList(ItemModel item, Map<int, Future<ItemModel>> itemMap) {
     final children = <Widget>[];
     children.add(buildTitle(item));
+    children.add(buildLink(item));
+    children.add(Divider(thickness: 5.0));
     final commentsList = item.kids.map((kidId) {
       return Comment(
         itemId: kidId,
@@ -60,7 +65,7 @@ class NewsDetail extends StatelessWidget {
   Widget buildTitle(ItemModel item) {
     return Container(
         alignment: Alignment.topCenter,
-        margin: EdgeInsets.all(10.0),
+        margin: EdgeInsets.fromLTRB(8.0, 10.0, 8.0, 10.0),
         child: Text(
           item.title,
           textAlign: TextAlign.center,
@@ -70,4 +75,27 @@ class NewsDetail extends StatelessWidget {
           ),
         ));
   }
+
+  Widget buildLink(ItemModel item) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ListTile(
+        title: Text("read story."),
+        leading: Icon(Icons.link),
+        onTap: () {
+          launchUrl(item.url);
+        },
+      ),
+    );
+  }
+
+  launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+
+  // Widget buildWebIcon(ItemModel item) {
+
+  // }
 }
